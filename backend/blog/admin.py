5d2d37582912bb2c9789f493158d24f89278fc61
lyf2000 <lyf2000@mail.ru>
2020-07-19 12:39:20 +0300

    add Blog, Post models

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from backend.blog.models import Blog, Post
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    # TODO add list of posts, filter
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class Post(models.Model):
+    blog = models.ForeignKey('Blog', on_delete=models.CASCADE, related_name='blogs')
+
+    title = models.CharField(max_length=256)
+    text = models.CharField(max_length=512)
