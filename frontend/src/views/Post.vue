43f2fd8dbfd37c7b5a80613c1b546bfcf7aa3c4e
lyf2000 <lyf2000@mail.ru>
2020-08-19 20:28:39 +0300

    add mrkdwn

--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -1,21 +1,29 @@
 <template>
    <div id="post">
-       <h1>qwgerhesthrtfh rthsrt hrthkrtphodrkphodfkgpb odfkgpbnodfkg pbnodfkgpbodfkgpbnfk</h1>
+      <v-textarea
+        background-color="light-blue"
+        v-model="text"
+      ></v-textarea>
+      <div>
+        <markdown-it-vue class="md-body" :content="text" />
+      </div>
   </div>
 </template>
 
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
+import MarkdownItVue from 'markdown-it-vue'
+import 'markdown-it-vue/dist/markdown-it-vue.css'
 
 
 @Component({
   components: {
-      
+      MarkdownItVue, 
   }
 })
 export default class Post extends Vue {
-
+    text = '## rget'
 }
 
 </script>
