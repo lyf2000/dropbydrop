7d663fb8f4143bd6ee5f6f28dd9178687633c336
lyf2000 <lyf2000@mail.ru>
2020-08-20 20:12:11 +0300

    add Home page content

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <markdown-it-vue  class="md-body my-5" :content="content" />    
     
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { Vue, Component } from 'vue-property-decorator';
+import MarkdownItVue from 'markdown-it-vue'
+import 'markdown-it-vue/dist/markdown-it-vue.css'
 
 
-export default {
-  name: 'Home',
+@Component({
   components: {
-    
-  },
-  
+      MarkdownItVue, 
+  }
+})
+export default class Post extends Vue {
+  content = `
+  # Привет!
+  ### **Сейчас Ты находишься на сайте, который выполняет вполне простую роль - он хранит и отображает записи.**  
+  ### **Записи - творение моих ума и души, каждая запись помогает мне лучше понимать себя и выражаться.**
+  ### **Ну, и конечно тут будут записи про :raised_hands:_IT_:raised_hands:**
+  ### **Немного про себя. Меня зовут Дамир, мне 20 лет и это один из способов моего самовыражения + удовлетворение своей личности как прагматика:)**
+  ### **У меня много в голове и на душе, что долго хранилось, и Я очень надеюсь(ХОЧУ), что смогу вылить сюда все без опаски, открыто**
+  ### **И в то же время, этот сайт поможет мне и во многих других сферах жизни, но в чем именно? Хех:grin:**
+  ### Если Ты хочешь чем-то поделиться, то вот мой [ТГ](https://telete.in/Planktoshka)
+  `
 }
 </script>
