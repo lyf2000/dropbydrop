0525f950d7c1182e9676bebf82b87217be0a6a21
lyf2000 <lyf2000@mail.ru>
2020-09-03 11:27:35 +0300

    remove vue from django

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.contrib.staticfiles.urls import static
+# from django.contrib.staticfiles.urls import static
 from django.conf import settings
 
 
-from django.views.static import serve as _serve
+# from django.views.static import serve as _serve
 
-def serve(request, path):
-    if '.' not in path:
-        path = 'index.html'
-    return _serve(request, path, '/vue')
+# def serve(request, path):
+#     if '.' not in path:
+#         path = 'index.html'
+#     return _serve(request, path, '/vue')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'(?P<path>(^/?$|.*\.(js|css)))', serve),
+    # re_path(r'(?P<path>(^/?$|.*\.(js|css)))', serve),
     # path('', TemplateView.as_view(template_name='base/index.html'), name='home'),
     path('api/', include('backend.users.urls', namespace='users')),
     path('api/', include('backend.blog.urls', namespace='blog')),
